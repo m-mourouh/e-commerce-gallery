1952a9fcbfa7afab9f1295309387c1c490f069cf
MOHAMED MOUROUH <60442896+m-mourouh@users.noreply.github.com>
2024-09-25 14:05:06 +0100

    Update router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,16 @@ const router = createRouter({
       component: HomeView,
       // Optional search query
       props: (route) => ({ search: route.query.search })
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../views/CartView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundView
     }
   ]
 })
