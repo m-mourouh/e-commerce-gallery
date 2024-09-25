d2f195688349b90de7b74b8bc1666e52d1196883
MOHAMED MOUROUH <60442896+m-mourouh@users.noreply.github.com>
2024-09-25 14:00:17 +0100

    Add EmptyCart component

---
+++ src/components/EmptyCart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex items-center flex-col">
+        <SfIconShoppingCart size="xl" />
+        <h1 class="text-2xl font-bold text-center mt-4">{{ $t('emptyCartMessage') }}</h1>
+        <SfButton size="sm" class="rounded-sm mt-10">
+        <router-link to="/" class="continue-shopping-btn">{{ $t('continueShopping') }}</router-link>
+      </SfButton>
+    </div>
+</template>
+
+<script setup>
+import {
+  SfButton,
+  SfIconShoppingCart,
+} from '@storefront-ui/vue'
+import { RouterLink } from 'vue-router';
+</script>
