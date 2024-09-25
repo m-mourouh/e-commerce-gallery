402ffd92b7944257535630d064b7d4a4150662c5
MOHAMED MOUROUH <60442896+m-mourouh@users.noreply.github.com>
2024-09-25 13:45:41 +0100

    Add ProductGallery component

---
+++ src/components/ProductGallery.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="grid grid-cols-2  md:grid-cols-4 lg:grid-cols-5 gap-4 mt-10">
+      <ProductCard v-for="product in products" :key="product?.id" :product="product" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import ProductCard from './ProductCard.vue';
+import type { Product } from '../types/Product';
+import { type PropType } from 'vue';
+
+defineProps({
+  products: {
+    type: Array as PropType<Product[]>,
+    required: true
+  }
+});
+
+</script>
