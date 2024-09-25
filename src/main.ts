d3f2185906dd56a47cdcb337a1f81150fd455919
MOHAMED MOUROUH <60442896+m-mourouh@users.noreply.github.com>
2024-09-25 13:53:02 +0100

    Refactor main.ts to use i18n for internationalization

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { store } from './store'
-
+import i18n from './locales/i18n'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://fakestoreapi.com'
 const app = createApp(App)
 
 
-app.use(store).use(router).use({
+app.use(store).use(router).use(i18n).use({
     install: (app) => {
         app.config.globalProperties.$axios = axios;
         app.provide('axios', axios);
